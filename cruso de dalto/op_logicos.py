8caae79cff945eef81086357e928a1cb9f652a76
Franco Furlani <francofurlani@gmail.com>
2023-04-26 22:59:22 -0300

    Co-authored-by: Franco Furlani <francofurlani@gmail.com>

---
+++ cruso de dalto/op_logicos.py
@@ -0,0 +1,20 @@
+#And
+
+resultado1 = True & True
+resultado2 = True & False
+resultado3 = False & True
+resultado4 = False & False
+
+# or 
+
+resultado5 = True | True
+resultado6 = True | False
+resultado7 = False | True
+resultado8 = False | False
+
+#not
+
+resultado9 = not True
+resultado10 = not False
+
+print(not resultado1)
