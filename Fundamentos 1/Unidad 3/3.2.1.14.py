77d3f2c3b6a888409ce48646a5ade2b15000f984
Franco Furlani <francofurlani@gmail.com>
2023-04-27 02:17:48 -0300

    nuevo comit
    Co-authored-by: Franco Furlani <francofurlani@gmail.com>

--- Fundamentos 1/Unidad 3/3.2.1.14.py
+++ Fundamentos 1/Unidad 3/3.2.1.14.py
@@ -1,7 +1,22 @@
-blocks = int(input("Ingresa el número de bloques: "))
+count = 0
+height =0
+def tomar_bloques():
+    
+    bloques = int(input("Ingresa el número de bloques: "))
 
-#
-# Escribe tu código aquí.
-#	
+    return bloques
 
-print("La altura de la pirámide:", height)
+def calcular_altura(blocks):
+
+    while blocks>0:
+        
+        count+=1
+        blocks-=count
+        if blocks >= height:
+            height+=1
+            
+    return height
+
+altura = calcular_altura(tomar_bloques())
+
+print("La altura de la pirámide:", altura)
