c0712de36b5a4bf7c338dd441a988e64c0e13175
Franco Furlani <francofurlani@gmail.com>
2023-05-19 00:24:09 -0300

    eliminacion

--- Fundamentos2/Unidad-3/objetos.py
+++
@@ -1,6 +0,0 @@
-class Stack:  # Definiendo la clase de la pila.
-    def __init__(self):  # Definiendo la función del constructor.
-        print("¡Hola!")
-
-
-stack_object = Stack()  # Instanciando el objeto.
