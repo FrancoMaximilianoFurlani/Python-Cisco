57b51002572b54ab43bc9f7d6005387481105f86
Franco Furlani <francofurlani@gmail.com>
2023-06-23 07:53:24 -0300

    005

--- Fundamentos2/Laboratorios/Furlani_Franco_3.2.1.15.py
+++ Fundamentos2/Laboratorios/Furlani_Franco_3.2.1.15.py
@@ -1,25 +1,25 @@
 # 3.2.1.15 Colas alias FIFO
 
-class Queue: # crea una lista de objestos 
+class Queue:  # crea una lista de objestos
     def __init__(self):
         self.stacklist = []
 
-    def put(self, elem): # carga los elementos en la lista
+    def put(self, elem):  # carga los elementos en la lista
         self.stacklist.append(elem)
 
-    def get(self): 
-        elem = self.stacklist[0] # toma el primer elemento de la lista lo asigna a una variable 
-        del self.stacklist[0] # borra el elemento de la lista 
-        return elem # retona la variable
+    def get(self):
+        # toma el primer elemento de la lista lo asigna a una variable
+        elem = self.stacklist[0]
+        del self.stacklist[0]  # borra el elemento de la lista
+        return elem  # retona la variable
 
 
-# esta clase estaba el princio del codigo para poder trabajarlo de manera organizana lo movimos aca 
+# esta clase estaba el princio del codigo para poder trabajarlo de manera organizana lo movimos aca
 
-class QueueError(Queue): # si cae en una lista vacia imprimira un mensaje de error
-    def __init__(self,Queue):
+class QueueError(IndexError):  # si cae en una lista vacia imprimira un mensaje de error
+    def __init__(self, Queue):
         Queue.__init__(self)
         print("Error de Cola")
-    
 
 
 que = Queue()
@@ -30,4 +30,5 @@ try:
     for i in range(4):
         print(que.get())
 except:
-    QueueError(Queue) # corregimos el codigo para que cuando ingrese un error ingrese a la clase QueueError
+    # corregimos el codigo para que cuando ingrese un error ingrese a la clase QueueError
+    QueueError(Queue)
