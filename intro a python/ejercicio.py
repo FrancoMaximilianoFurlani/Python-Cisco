e924e1cdf26fccd8b17cd01cfd7a7d809cbc1d2f
FrancoMaximilianoFurlani <116849516+FrancoMaximilianoFurlani@users.noreply.github.com>
2024-01-31 19:34:47 -0300

    juego de palabras

---
+++ intro a python/ejercicio.py
@@ -0,0 +1,10 @@
+lista=['hola', 'mugel', 'elemento', 'perrito', 'casa']
+
+n = input('ingrese una palabra')
+
+for i in lista:
+    if i == n:
+        print ('tu para la palabraesta en la lista')
+        break
+
+print (' fin del juego')
